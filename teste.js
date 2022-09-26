0a6f1e3a3cb1a727e06423566461efdef2a46c36
jonathagus <jonathabs333@gmail.com>
2022-09-26 15:29:25 -0300

    segundo commit

---
+++ teste.js
@@ -0,0 +1,13 @@
+//notas dos Bimestres do aluno
+var bim1 =5;
+var bim2 =8;
+var bim3 =5;
+var bim4 =9;
+
+var media = bim1 + bim2 + bim3 + bim4/4
+
+if(media >=7 ){
+    console.log("Aprovado!" )
+}else{
+    console.log("Reprovado." )
+}
